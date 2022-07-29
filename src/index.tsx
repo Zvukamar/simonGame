218bdf67f5238e1d827db1caeaedf59be7da78b1
Havkin, Alexander (ah476f) <ah476f@intl.att.com>
2022-07-29 18:58:43 +0300

    added redux-persist to the project

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import { SafeAreaView, StyleSheet } from 'react-native';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 import Navigator from './navigation';
-import { store } from './store';
+import { store, persistor } from './store';
 
 const App = () => {
   return (
     <Provider store={store}>
-      <SafeAreaView style={styles.container}>
-        <Navigator />
-      </SafeAreaView>
+      <PersistGate loading={null} persistor={persistor}>
+        <SafeAreaView style={styles.container}>
+          <Navigator />
+        </SafeAreaView>
+      </PersistGate>
     </Provider>
   );
 };
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,21 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import AsyncStorage from '@react-native-community/async-storage';
+import { persistStore, persistReducer } from 'redux-persist';
+import thunk from 'redux-thunk';
 import globalReducer from './globalSlice';
 
-export const store = configureStore({
-  reducer: {
-    global: globalReducer,
-  },
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage,
+};
+
+const rootReducer = combineReducers({
+  global: persistReducer(persistConfig, globalReducer),
 });
+
+export const store = configureStore({
+  reducer: rootReducer,
+  middleware: [thunk],
+});
+
+export const persistor = persistStore(store);
